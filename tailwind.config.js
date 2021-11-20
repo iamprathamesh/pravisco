6f1af146fce4c858e41b5374fcafbf9795ac4ffd
Prathamesh Madur <prathameshmadur11@gmail.com>
2021-11-20 09:15:42 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  prefix: '',
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  }
+};
