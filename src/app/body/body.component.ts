c3e5e0e5addcea8f30f58a3afcc081b56cfb20c5
Prathamesh Madur <prathameshmadur11@gmail.com>
2021-11-20 23:44:07 +0530

    added carousel

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,8 +7,22 @@ import { Component, OnInit } from "@angular/core";
 })
 export class BodyComponent implements OnInit {
 
+  carousels: string[] = ["../../assets/images/png/carousel-1.png", "../../assets/images/png/carousel-2.png", "../../assets/images/png/carousel-3.png", "../../assets/images/png/carousel-4.png", "../../assets/images/png/carousel-5.png"]
+  carouselImage: string = this.carousels[0];
+  fade: boolean = false;
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    let counter = 1;
+    setInterval(async () => {
+      this.fade = true;
+      setTimeout(() => {
+        this.carouselImage = this.carousels[counter % this.carousels.length];
+        counter++;
+        this.fade = false;
+      }, 250);
+    }, 3000);
+  }
 
 }
