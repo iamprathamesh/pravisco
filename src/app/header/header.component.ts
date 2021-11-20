48fc43cbd9410f3da0b361d27418147d46d30696
jatinvyas1 <jatin.vyas@outlook.com>
2021-11-20 13:53:35 +0530

    update header mobile menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ['header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  showMobileMenu: boolean = false;
   constructor() { }
 
   ngOnInit() { }
+
+  closeMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
 }
