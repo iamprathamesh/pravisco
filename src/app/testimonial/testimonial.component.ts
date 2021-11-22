5ec5e97040b5f539fc4e3e3fe7f306dfec4b020b
Prathamesh Madur <prathameshmadur11@gmail.com>
2021-11-22 20:41:47 +0530

    Added stats

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -21,20 +21,19 @@ export class TestimonialComponent implements OnInit {
 
   testimonials: Testmonial[] = [
     {
-      testimonial: `We are very happy with pravisco.com; very serious and consistent in their work.
-      Vishwas and his team of programmers have always been there for us all the time. This is a company I can recommend to anyone
-      to perform any work.`, name: "Alex Smith", img: "../../assets/images/png/alex.jpg"
-    },
-    {
       testimonial: `We would like to express our satisfaction on the co-operation regarding the development of our web application.
     Prathamesh and the development team did a very professional job. We are satisfied with the solution given to us
     and with the communication flow through the project.`, name: "Marlies Williams", img: "../../assets/images/png/marlies.jpg"
     },
     {
+      testimonial: `We are very happy with pravisco.com; very serious and consistent in their work.
+      Vishwas and his team of programmers have always been there for us all the time. This is a company I can recommend to anyone
+      to perform any work.`, name: "Alex Smith", img: "../../assets/images/png/alex.jpg"
+    },
+    {
       testimonial: ` I wanted to take a moment to thank you for the services your team has provided.
       Your team has been a pleasure to work with, professional and timely. The only delay in work that
-      we have experienced has been due to our own lack of organization managing our projects, not yours.
-      Job well done and I hope we can continue to grow together.`, name: "John Blake", img: "../../assets/images/png/john_blake.jpg"
+      we have experienced has been due to our own lack of organization managing our projects, not yours.`, name: "John Blake", img: "../../assets/images/png/john_blake.jpg"
     }
   ]
 
